d4adc55b3bd7a8c47d71936577bdb89cdb6d4c6f
BIT Student <classroom@bgit.rs>
2018-05-25 16:26:27 +0200

    bit show nastavak

--- V2/bit-show/js/main.js
+++ V2/bit-show/js/main.js
@@ -1,111 +1,58 @@
-// const dataModule = (() => {
-//   sendRequest = () => {
-//     var request = $.ajax({
-//       url: "http://api.tvmaze.com/shows",
-//       method: "GET"
-//     });
-//     request.done((response) => {
-//       console.log(response);
-    
-//       const body = $("body");
-//     const container = $(".container");
-//     const row = document.createElement("div");
-//     $(row).addClass("row");
-//     const col = document.createElement("div");
-//     $(col).addClass("col-4");
-//     const showTitle = document.createElement("h5");
-    
 
-//     const searchInput = document.querySelector("#searchInput");
-// for(let i = 0; i < response.length / 20; i++) {
-//     showTitle.textContent = response[i].id;
-//     let imageBig = document.createElement("img")
-//     let imageSrc = response[i].image.original;
-//     imageBig.setAttribute("src", imageSrc)
-//     col.append(imageBig)
-//     col.append(showTitle);
-//     row.append(col);
-//     container.append(row);
-// }
-        
-//         body.append(container)
-//     });
-//   };
-//   return {
-//     sendRequest
-//   };
-// })();
-
-// const uiModule = (() => {
-//   collectData = () => {
-//     const body = $("body");
-//     const container = $(".container");
-//     const row = document.createElement("div");
-//     $(row).addClass("row");
-//     const col = document.createElement("div");
-//     $(col).addClass("col-4");
-//     const showTitle = document.createElement("h5");
-
-//     const searchInput = document.querySelector("#searchInput");
-//   };
-//   return {
-//     collectData
-//   };
-// })();
-
-// const mainModule = ((data, ui) => {
-//   const searchBtn = document.querySelector("#searchBtn");
-//   // init = (handleRequest) => {
-//   // searchBtn.addEventListener("click", dataModule.sendRequest)
-//   // }
-//   // init()
-//   dataModule.sendRequest();
-// })(dataModule, uiModule);
 
 const dataModule = (() => {
-  
-    sendRequest = (successHandler) => {
+  sendRequest = successHandler => {
     var request = $.ajax({
       url: "http://api.tvmaze.com/shows",
       method: "GET"
     });
-    request.done((response) => {
-      console.log(response); 
-        successHandler(response)
+    request.done(response => {
+      successHandler(response);
+      response.sort((curr, next) => {
+          if(next.rating.average > curr.rating.average) {
+            return 1;
+          }
+          if(next.rating.average < curr.rating.average) {
+            return -1;
+          }
+          if(next.rating.average == curr.rating.average) {
+            return 0;
+          }
+          console.log(response)
+      })
     });
+    
   };
+
+
   return {
     sendRequest
   };
 })();
 
 const uiModule = (() => {
-
-  displayData = (response) => {
     const body = $("body");
-    const container = $(".container");
-    const row = document.createElement("div");
-   
-
+    const $container = $(".container");
     const searchInput = document.querySelector("#searchInput");
-    for(let i = 0; i < 50; i++) {
-        $(row).addClass("row");
-        const col = document.createElement("div");
-        $(col).addClass("col-4");
-        const link = document.createElement("a")
-        link.setAttribute("href", "#")
-        const showTitle = document.createElement("h5");
-        link.appendChild(showTitle)
-        showTitle.textContent = response[i].name;
-        let imageBig = document.createElement("img")
-        let imageSrc = response[i].image.original;
-        imageBig.setAttribute("src", imageSrc)
-        col.append(imageBig)
-        col.append(showTitle);
-        row.append(col);
-        container.append(row);
+
+  displayData = response => {
+    const $row = $("<div class='row'>");
+
+    for (let i = 0; i < 50; i++) {
+        const $card = $(`
+            <div class='col-4'>
+                <img src='${response[i].image.original}'>
+                <a class='showTitle' href='./single.html'><h5>${response[i].name}</h5></a>
+            </div>`
+        );
+
+        $row.append($card);
     }
-  }
+    
+    $container.append($row);
+    console.log(response)
+  };
+
   return {
     displayData
   };
@@ -114,9 +61,7 @@ const uiModule = (() => {
 const mainModule = ((data, ui) => {
   const searchBtn = document.querySelector("#searchBtn");
 
-  dataModule.sendRequest(function (response) {
-      uiModule.displayData(response)
-      
+  dataModule.sendRequest(function(response) {
+    uiModule.displayData(response);
   });
-
 })(dataModule, uiModule);
