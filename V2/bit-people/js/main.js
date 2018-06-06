597454b34a5b773047b7c410a7b82433957b0f27
BIT Student <classroom@bgit.rs>
2018-06-06 12:53:11 +0200

    bit people switch between list and grid view

--- V2/bit-people/js/main.js
+++ V2/bit-people/js/main.js
@@ -1,16 +1,29 @@
 const dataModule = (() => {
 
     class Person {
-        constructor(name, surname, email, phone, photo) {
+        constructor(name, surname, email, phone, photo, avatar, birthday) {
             this.name = name
             this.surname = surname
             this.email = email
             this.phone = phone
             this.photo = photo
+            this.avatar = avatar
+            this.birthday = `${new Date(birthday).getDate()}.${new Date(birthday).getMonth() - 1}.${new Date(birthday).getFullYear()}`
+        }
+        formatMail() {
+            let splittedMail = this.email.split("@");
+            let mailFirstPart = splittedMail[0];
+            let formatedFirstPartMail = "";
+            for (let i = 0; i < mailFirstPart.length; i++) {
+                formatedFirstPartMail = `${mailFirstPart[0]}${mailFirstPart[1]}${mailFirstPart[2]}...${mailFirstPart[mailFirstPart.length - 3]}${mailFirstPart[mailFirstPart.length - 2]}${mailFirstPart[mailFirstPart.length - 1]}`;
+            }
+            let formatedSecondPartMail = `@${splittedMail[1]}`
+            let formatedMail = `${formatedFirstPartMail}${formatedSecondPartMail}`
+            return formatedMail;
         }
     }
 
-    fetchUserList = (successHandler) => {
+    fetchUserList = (successHandlerGrid) => {
         let request = $.ajax({
             url: "https://randomuser.me/api/?results=15",
             method: "GET"
@@ -22,15 +35,14 @@ const dataModule = (() => {
             const myUsersList = []
 
             for (let i = 0; i < usersData.length; i++) {
-                const {name, email, phone, picture} = usersData[i];
+                const { name, email, phone, picture } = usersData[i];
                 const userDatObj = usersData[i];
-                const user = new Person(userDatObj.name.first, userDatObj.name.last, userDatObj.email, userDatObj.phone, userDatObj.picture.large)
+                const user = new Person(userDatObj.name.first, userDatObj.name.last, userDatObj.email, userDatObj.phone, userDatObj.picture.large, userDatObj.picture.thumbnail, userDatObj.dob)
                 myUsersList.push(user)
             }
 
-            console.log("myUsersList", myUsersList);
-            
-            successHandler(myUsersList)
+            console.log("response", response);
+            successHandlerGrid(myUsersList)
         });
     }
 
@@ -43,6 +55,7 @@ const uiModule = (() => {
     const body = $("body");
     const $container = $(".container");
     const $row = $(".row")
+    const $list = $("#listContent")
 
     const createCardItem = (person) => {
         return $(`
@@ -54,8 +67,7 @@ const uiModule = (() => {
                 </div>
                 <div class="card-content">
                 <p>
-                Email: ${person.email}
-                \n
+                Email: ${person.formatMail()} </br>
                 Phone: ${person.phone}
                 </p>
                 </div>
@@ -64,9 +76,32 @@ const uiModule = (() => {
         `)
     }
 
+    const createListItem = (person) => {
+        return $(`
+        <li class="collection-item avatar">
+      <img src="${person.avatar}" alt="" class="circle">
+      <span class="title">${person.name} ${person.surname}</span>
+      <p><i class="tiny material-icons">email</i> ${person.formatMail()}<br>
+      <i class="tiny material-icons">cake</i> ${person.birthday}
+      </p>
+    </li>
+        `)
+    }
+
+    const displayDataList = (personList) => {
+        for (let i = 0; i < 15; i++) {
+            const person = personList[i]
+            const $listItem = createListItem(person)
+
+            $list.append($listItem)
+        }
+        $container.append($list)
+    }
+
+
     const displayDataGrid = (personList) => {
         console.log("personList", personList);
-        
+
         for (let i = 0; i < 15; i++) {
             const person = personList[i]
             const $card = createCardItem(person)
@@ -77,22 +112,59 @@ const uiModule = (() => {
         console.log(personList)
     }
 
+  
     return {
+        displayDataList,
         displayDataGrid
     }
 })();
 
 const mainModule = ((data, ui) => {
-    const initDataGrid = () => {
+    const $gridBtn = $("#gridBtn");
+    const $listBtn = $("#listBtn");
+    const $gridContent = $("#gridContent");
+    const $listContent = $("#listContent");
+    const hiddenClass = "hidden";
 
+    const toggleBtn = (event) => {
+        event.stopPropagation();
+        event.preventDefault();
+
+        if ($gridBtn.hasClass(hiddenClass)) {
+            $gridBtn.removeClass(hiddenClass);
+            $listBtn.addClass(hiddenClass);
+            $gridContent.removeClass(hiddenClass);
+            $listContent.addClass(hiddenClass);
+            return;
+        }
+
+        if ($listBtn.hasClass(hiddenClass)) {
+            $listBtn.removeClass(hiddenClass);
+            $gridBtn.addClass(hiddenClass);
+            $listContent.removeClass(hiddenClass);
+            $gridContent.addClass(hiddenClass);
+        }
+    }
+
+    const initDataList = () => {
+        data.fetchUserList(function (userList) {
+            ui.displayDataList(userList)
+            $listBtn.click(toggleBtn)
+        })
+    }
+
+    const initDataGrid = () => {
         data.fetchUserList(function (userList) {
             console.log("userList", userList);
             ui.displayDataGrid(userList)
+            $gridBtn.click(toggleBtn)
         })
-
     }
 
+    
+
     return {
+        initDataList,
         initDataGrid
     }
 })(dataModule, uiModule);
