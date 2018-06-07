76ce97651469941c68b7c7c6faae3d9de48e7bc6
BIT Student <classroom@bgit.rs>
2018-06-07 15:58:27 +0200

    bit-people progress

--- V2/bit-people/js/main.js
+++ V2/bit-people/js/main.js
@@ -1,3 +1,4 @@
+//--------DATA MODULE-------------
 const dataModule = (() => {
 
     class Person {
@@ -51,13 +52,14 @@ const dataModule = (() => {
         fetchUsers
     }
 })();
-
+//--------------UI MODULE-------------------------------------
 const uiModule = (() => {
     const body = $("body");
     const $container = $(".container");
     const $row = $(".row")
     const $list = $("#listContent")
 
+    //------------------------------Create Card Item----------------------------
     const createCardItem = (person) => {
         const $cardItem = $(`
         <div class="col s4">
@@ -75,12 +77,12 @@ const uiModule = (() => {
         </div>
         </div>
     `)
-    if(person.gender === "female") {
-        $cardItem.find(".card-content").css("background-color", "#ffebee")
-    }
+        if (person.gender === "female") {
+            $cardItem.find(".card-content").css("background-color", "#ffebee")
+        }
         return $cardItem;
     }
-
+    //------------------------------Create List Item----------------------------
     const createListItem = (person) => {
         const $listItem = $(`
         <li class="collection-item avatar">
@@ -93,13 +95,15 @@ const uiModule = (() => {
       </div>
     </li>
         `)
-        if(person.gender === "female") {
+        if (person.gender === "female") {
             $listItem.css("background-color", "#ffebee")
         }
         return $listItem;
     }
 
+        //------------------------------Display List----------------------------
     const displayDataList = (personList) => {
+        
         for (let i = 0; i < 15; i++) {
             const person = personList[i]
             const $listItem = createListItem(person)
@@ -109,7 +113,7 @@ const uiModule = (() => {
         $container.append($list)
     }
 
-
+        //------------------------------Display Grid----------------------------
     const displayDataGrid = (personList) => {
         console.log("personList", personList);
 
@@ -123,13 +127,26 @@ const uiModule = (() => {
         console.log(personList)
     }
 
-  
+    // const createLoadingPage = () => {
+    //     $(document).ready(() => {
+    //         $(document).ajaxStart(() => {
+    //             $("#wait").css("display", "block");
+    //         });
+    //         $(document).ajaxComplete(() => {
+    //             $("#wait").css("display", "none");
+    //         });
+    //         $("#refreshBtn").click(() => {
+    //             $("#txt").load("index.html")
+    //         });
+    //     })
+    // }
+
     return {
         displayDataList,
         displayDataGrid
     }
 })();
-
+//----MAIN MODULE------------------
 const mainModule = ((data, ui) => {
     const $gridBtn = $("#gridBtn");
     const $listBtn = $("#listBtn");
@@ -137,40 +154,94 @@ const mainModule = ((data, ui) => {
     const $listContent = $("#listContent");
     const hiddenClass = "hidden";
 
-    const toggleBtn = (event) => {
-        event.stopPropagation();
-        event.preventDefault();
+    // const toggleBtn = (event) => {
+    //     event.stopPropagation();
+    //     event.preventDefault();
 
-        if ($gridBtn.hasClass(hiddenClass)) {
-            localStorage.setItem('layout', 'grid');
+    //     if ($gridBtn.hasClass(hiddenClass)) {
+    //         localStorage.setItem('layout', 'grid');
 
-            $gridBtn.removeClass(hiddenClass);
-            $listBtn.addClass(hiddenClass);
-            $gridContent.removeClass(hiddenClass);
-            $listContent.addClass(hiddenClass);
-            return;
-        }
+    //         $gridBtn.removeClass(hiddenClass);
+    //         $listBtn.addClass(hiddenClass);
+    //         $gridContent.removeClass(hiddenClass);
+    //         $listContent.addClass(hiddenClass);
+    //         return;
+    //     }
 
-        if ($listBtn.hasClass(hiddenClass)) {
-            localStorage.setItem('layout', 'list');
+    //     if ($listBtn.hasClass(hiddenClass)) {
+    //         localStorage.setItem('layout', 'list');
 
-            $listBtn.removeClass(hiddenClass);
-            $gridBtn.addClass(hiddenClass);
-            $listContent.removeClass(hiddenClass);
-            $gridContent.addClass(hiddenClass);
-        }
-    }
-    
+    //         $listBtn.removeClass(hiddenClass);
+    //         $gridBtn.addClass(hiddenClass);
+    //         $listContent.removeClass(hiddenClass);
+    //         $gridContent.addClass(hiddenClass);
+    //     }
+    // }
+
+    //------------------------------Init DIsplay---------------------------
     const initAllUsersData = () => {
+        let listLayout = localStorage.getItem("listLayout") === 'true';
+
         data.fetchUsers(function (userList) {
-            ui.displayDataList(userList)
-            ui.displayDataGrid(userList)
-            $listBtn.click(toggleBtn)
-            $gridBtn.click(toggleBtn)
+
+            const listSetStorage = () => {
+                listLayout = true;
+                localStorage.setItem("listLayout", listLayout)
+
+                ui.displayDataList(userList)
+                $gridContent.remove()
+                // $listBtn.off("click", listSetStorage)
+                $gridBtn.click(gridSetStorage)
+
+                $gridBtn.removeClass(hiddenClass);
+                $listBtn.addClass(hiddenClass);
+            }
+
+            const gridSetStorage = () => {
+                listLayout = false;
+                $gridContent.text("");
+                localStorage.setItem("listLayout", listLayout)
+
+                ui.displayDataGrid(userList)
+                $listContent.remove()
+                // $gridBtn.off("click", gridSetStorage)
+                $listBtn.click(listSetStorage)
+
+                $listBtn.removeClass(hiddenClass);
+                $gridBtn.addClass(hiddenClass);
+              
+            }
+
+            $gridBtn.click(gridSetStorage)
+            $listBtn.click(listSetStorage)
+            // $gridBtn.off("click", gridSetStorage)
+            // $listBtn.off("click", listSetStorage)
+
+            // $listBtn.click(toggleBtn)
+            // $gridBtn.click(toggleBtn)
+            if (listLayout) {
+                listSetStorage();
+            } else {
+                gridSetStorage();
+            }
         })
     }
+//----------------Filter Users----------------------------
+    // const filterUsers = () => {
+    //     $(document).ready(() => {
+    //         $searchInput = $(".searchInput");
+    //         $searchInput.on("keyUp", () => {
+    //             let value = $(this).val().toLowerCase();
+    //             $($list).filter(() => {
+    //                 $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    //             })
+    //         })
+    //     })
+    // }
+
     return {
-        initAllUsersData   
+        initAllUsersData,
+        // filterUsers
     }
 })(dataModule, uiModule);
 
